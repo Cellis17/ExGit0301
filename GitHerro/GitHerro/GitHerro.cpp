c21f69b7368ec12023eab4d3e2bb5c41145e19b1
Caleb Ellis <cellis0611@vbtc.net>
2017-03-20 09:16:22 -0400

    Init Commit

---
+++ GitHerro/GitHerro/GitHerro.cpp
@@ -0,0 +1,15 @@
+// GitHerro.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Herro" << endl;
+	system("pause");
+	return 0;
+}
+

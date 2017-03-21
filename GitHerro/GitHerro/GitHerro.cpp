92a7944c7b8ab074793aad4299df4c3986673757
Devin Burt <dburt0444@vbtc.net>
2017-03-21 09:28:13 -0400

    .

--- GitHerro/GitHerro/GitHerro.cpp
+++ GitHerro/GitHerro/GitHerro.cpp
@@ -1,18 +1,3 @@
-// GitHerro.cpp : Defines the entry point for the console application.
-//
-
-#include <iostream>
-
-using namespace std;
-
-
-int main()
-{
-	cout << "Herro" << endl;
-	system("pause");
-	return 0;
-}
-
 string mixUp(const string& aWord)
 {
 	string aJumble = aWord;

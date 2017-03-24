43e46ad867d1d42f49ce1e8aa7bfad9ef9579fcc
Blake Wilson <bwilson0584@vbtc.net>
2017-03-24 09:38:05 -0400

    Hello

--- GitHerro/GitHerro/GitHerro.cpp
+++
@@ -1,8 +0,0 @@
-void welcomePlayer(const string& aJumble)
-{
-	cout << "t\t\tWelcome to World Jumble !" << endl;
-	cout << "Unscramble the letters to make a word." << endl;
-	cout << " Enter quit ' to quite the game. " << endl;
-	cout << "The jumble is: " << aJumble << endl << endl;
-}
-

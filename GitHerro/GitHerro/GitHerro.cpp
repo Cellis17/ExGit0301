55af130440c8559f316113d599762cafd5e43ae7
Blake Wilson <bwilson0584@vbtc.net>
2017-03-24 10:03:09 -0400

    new .gitignore and *.cpp

---
+++ GitHerro/GitHerro/GitHerro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
+void welcomePlayer(const string& aJumble)
+{
+	cout << "t\t\tWelcome to World Jumble !" << endl;
+	cout << "Unscramble the letters to make a word." << endl;
+	cout << " Enter quit ' to quite the game. " << endl;
+	cout << "The jumble is: " << aJumble << endl << endl;
+}
+
